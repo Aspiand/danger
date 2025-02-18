50a409619fe158126a0d932fab6fadbbad71f37d
Aspian <p.aspian1738@gmail.com>
2025-02-19 00:48:11 +0800

    delphi(klinik): pasien

---
+++ Delphi/klinik2/Project.dpr
@@ -0,0 +1,14 @@
+program Project;
+
+uses
+  Vcl.Forms,
+  Unith in 'Unith.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
